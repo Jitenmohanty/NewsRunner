3d2f957d7157b711eecf19de01dfee951c7b7655
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-05-16 12:22:39 +0530

    Manually Fecth data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react'
 import Navbars from './Components/Navbars'
-import NewsItem from './Components/NewsItem'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <Navbars/>
-        <div className="row">
-          <div className="col-md-4">
-        <NewsItem/>
-          </div>
-          <div className="col-md-4">
-        <NewsItem tittle="myTitle" description="desc"/>
-          </div>
-          <div className="col-md-4">
-        <NewsItem/>
-          </div>
-          <div className="col-md-4">
-        <NewsItem/>
-          </div>
-        </div>
-      </div>
+      <div>
+      <Navbars/>
+      <News/>
+    </div>
     )
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 
-export default class NewsItem extends Component {
+ class NewsItem extends Component {
     render() {
-        let {tittle,description} = this.props;
+        let {tittle,description,imgUrl,newsUrl} = this.props;
         return (
-          <div>
-                <div className="card" style={{width: "18rem"}}>
-                     <img src="https://ichef.bbci.co.uk/news/1024/branded_news/00BF/production/_129719100_c8e05c9169498a3547ddd1cc67dfe1677693c18d0_226_3072_17283072x1728-1.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{tittle}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+          <div className="my-3">
+          <div className="card" style={{width: "18rem"}}>
+              <img src={imgUrl} className="card-img-top" alt="..."/>
+              <div className="card-body">
+                  <h5 className="card-title">{tittle}</h5>
+                  <p className="card-text">{description}...</p>
+                  <a href={newsUrl} className="btn btn-sm btn-primary" target='_blank'>Read More</a>
+              </div>
           </div>
+      </div>
         )
       }
 }
+
+export default NewsItem;
